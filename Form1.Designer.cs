a6b984e94450898dc42271545e84eba45e9ab43c
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2023-12-13 06:36:15 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ImageClassificationTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_RunTrain = new System.Windows.Forms.Button();
+            this.button_RunClassification = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox_backboneName = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_RunTrain
+            // 
+            this.button_RunTrain.Location = new System.Drawing.Point(458, 85);
+            this.button_RunTrain.Name = "button_RunTrain";
+            this.button_RunTrain.Size = new System.Drawing.Size(107, 31);
+            this.button_RunTrain.TabIndex = 0;
+            this.button_RunTrain.Text = "Run";
+            this.button_RunTrain.UseVisualStyleBackColor = true;
+            this.button_RunTrain.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_RunClassification
+            // 
+            this.button_RunClassification.Location = new System.Drawing.Point(458, 70);
+            this.button_RunClassification.Name = "button_RunClassification";
+            this.button_RunClassification.Size = new System.Drawing.Size(107, 34);
+            this.button_RunClassification.TabIndex = 1;
+            this.button_RunClassification.Text = "Run";
+            this.button_RunClassification.UseVisualStyleBackColor = true;
+            this.button_RunClassification.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "data dir";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "backbone";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox_backboneName);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button_RunTrain);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(588, 128);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Train";
+            // 
+            // comboBox_backboneName
+            // 
+            this.comboBox_backboneName.FormattingEnabled = true;
+            this.comboBox_backboneName.Items.AddRange(new object[] {
+            "AlexNet",
+            "DenseNet",
+            "GoogLeNet",
+            "MobilenetV2",
+            "NiN",
+            "ResNet",
+            "VGG"});
+            this.comboBox_backboneName.Location = new System.Drawing.Point(20, 85);
+            this.comboBox_backboneName.Name = "comboBox_backboneName";
+            this.comboBox_backboneName.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_backboneName.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "modelName";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_RunClassification);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(587, 110);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Classification";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 294);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ImageClassificationTest";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_RunTrain;
+        private System.Windows.Forms.Button button_RunClassification;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox_backboneName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+using SciSharp.Models.ImageClassification;
+using SciSharp.Models.ImageClassification.Zoo;
+
+using WinFormStringCnvClass;
+
+namespace ImageClassificationTest
+{
+    public partial class Form1 : Form
+    {
+        IModelZoo model;
+        string thisExeDirPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            thisExeDirPath = Path.GetDirectoryName(Application.ExecutablePath);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+            if (File.Exists(paramFilename))
+            {
+                WinFormStringCnv.setControlFromString(this, File.ReadAllText(paramFilename));
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string FormContents = WinFormStringCnv.ToString(this);
+            string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+            File.WriteAllText(paramFilename, FormContents);
+        }
+
+        private (FolderClassificationConfig config, int img_size) GetConfig()
+        {
+            var config = new FolderClassificationConfig();
+            config.BaseFolder = @"R:\";
+            config.DataDir = "data";
+            var img_size = 256;
+
+            config.InputShape = (img_size, img_size);
+            config.BatchSize = 2;
+
+            return (config, img_size);
+        }
+
+
+        private void LoadModel()
+        {
+            if (comboBox_backboneName.Text == "AlexNet") model = new AlexNet();
+            else if (comboBox_backboneName.Text == "DenseNet") model = new DenseNet();
+            else if (comboBox_backboneName.Text == "GoogLeNet") model = new GoogLeNet();
+            else if (comboBox_backboneName.Text == "MobilenetV2") model = new MobilenetV2();
+            else if (comboBox_backboneName.Text == "NiN") model = new NiN();
+            else if (comboBox_backboneName.Text == "ResNet") model = new ResNet();
+            else if (comboBox_backboneName.Text == "VGG") model = new VGG();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var (config, img_size) = GetConfig();
+
+            config.BatchSize = 24;
+            config.ValidationStep = 5;
+            config.Epoch = 20;
+
+            LoadModel();
+
+            var classifier = new FolderClassification(config, model);
+
+            config.WeightsPath = $"{model.GetType().Name}_{img_size}x{img_size}_weights.ckpt";
+
+            classifier.Train();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var (config, img_size) = GetConfig();
+
+            LoadModel();
+
+            var classifier = new FolderClassification(config, model);
+
+            config.WeightsPath = $"{model.GetType().Name}_{img_size}x{img_size}_weights.ckpt";
+
+            var imageFile = Path.Combine(config.BaseFolder, "data", "class_a", "000000.png");
+            var result = classifier.Predict(imageFile);
+            label1.Text = ($"{result.Label}({result.Probability})");
+        }
+
+
+    }
+}
