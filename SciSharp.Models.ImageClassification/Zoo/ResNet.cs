8f4effcff5e73f30873e633b1a2f325f7d6eaaaa
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2023-12-18 03:59:10 +0900

    Add files via upload

--- SciSharp.Models.ImageClassification/Zoo/ResNet.cs
+++ SciSharp.Models.ImageClassification/Zoo/ResNet.cs
@@ -10,9 +10,15 @@ namespace SciSharp.Models.ImageClassification.Zoo
 {
     public class ResNet : IModelZoo
     {
+        /*
         class Residual : Layer
         {
-            public static int layerId;
+            static int layerId;
+
+            static public void Clear_session()
+            {
+                layerId = 0;
+            }
 
             ILayer conv1;
             ILayer conv2;
@@ -20,6 +26,7 @@ namespace SciSharp.Models.ImageClassification.Zoo
             ILayer bn1;
             ILayer bn2;
 
+            
             public Residual(int num_channels, bool use_1x1conv = false, int strides = 1) : base(new LayerArgs { Name = "Residual_" + ++layerId })
             {
                 // print($"name: {Name} num_channels:{num_channels} firstblock:{use_1x1conv} strides:{strides}");
@@ -66,10 +73,16 @@ namespace SciSharp.Models.ImageClassification.Zoo
                 return tf.nn.relu(Y);
             }
         }
-
+        */
+        /*
         public class ResnetBlock : Layer
         {
-            public static int layerId;
+            static int layerId;
+
+            static public void Clear_session()
+            {
+                layerId = 0;
+            }
 
             public ResnetBlock(int num_channels, int num_residuals, bool first_block = false) : base(new LayerArgs { Name = "ResnetBlock_" + ++layerId })
             {
@@ -97,33 +110,128 @@ namespace SciSharp.Models.ImageClassification.Zoo
                 return X;
             }
         }
+        */
 
+        public Tensors Residual(int num_channels, Tensors input, bool use_1x1conv = false, int strides = 1)
+        {
+            Tensors x = input;
+            /*
+            // print($"name: {Name} num_channels:{num_channels} firstblock:{use_1x1conv} strides:{strides}");
+            conv1 = keras.layers.Conv2D(num_channels, kernel_size: 3, strides: strides, padding: "same", activation: "relu");
+            conv2 = keras.layers.Conv2D(num_channels, kernel_size: 3, padding: "same", activation: "relu");
+            Layers.add(conv1);
+            Layers.add(conv2);*/
+
+            x = keras.layers.Conv2D(num_channels, kernel_size: 3, strides: strides, padding: "same", activation: "relu").Apply(x);
+            x = keras.layers.Conv2D(num_channels, kernel_size: 3, padding: "same", activation: "relu").Apply(x);
+
+            if (use_1x1conv)
+            {
+                /*conv3 = keras.layers.Conv2D(num_channels, kernel_size: 1, strides: strides, activation: "relu");
+                Layers.add(conv3);*/
+                x = keras.layers.Conv2D(num_channels, kernel_size: 1, strides: strides, activation: "relu").Apply(x);
+
+            }
+            /*
+                        bn1 = keras.layers.BatchNormalization();
+                        bn2 = keras.layers.BatchNormalization();
+
+                        Layers.add(bn1);
+                        Layers.add(bn2);
+                        */
+
+            x = keras.layers.BatchNormalization().Apply(x);
+            x = keras.layers.BatchNormalization().Apply(x);
+
+            return x;
+        }
+
+
+        public Tensors ResnetBlock(int num_channels, int num_residuals, Tensors input, bool first_block = false)
+        {
+            Tensors x = input;
+
+            for (int i = 0; i < num_residuals; i++)
+            {
+                if (i == 0 && first_block == false)
+                {
+                    x = Residual(num_channels, x, use_1x1conv: true, strides: 2);
+                    //Layers.add(new Residual(num_channels, use_1x1conv: true, strides: 2));
+                }
+                else
+                {
+                    x = Residual(num_channels, x);
+                    //Layers.add(new Residual(num_channels));
+                }
+            }
+
+            return x;
+        }
+
+
+
+        public void Clear_session()
+        {
+            
+            keras.backend.clear_session();
+            //ResnetBlock.Clear_session();
+            //Residual.Clear_session();
+        }
 
         public IModel BuildModel(FolderClassificationConfig config)
         {
-            keras.backend.clear_session();
-            Residual.layerId = 0;
-            ResnetBlock.layerId = 0;
+            Clear_session();
 
-            
+            Shape input_shape = (config.InputShape[0], config.InputShape[1], 3);
+            int num_classes = config.NumberOfClass;
+
+            // Define the input tensor
+            Tensors inputs = keras.Input(input_shape);
+            Tensors x = inputs;
+
+            /*
             var b1 = new BlocksLayer(new[] {
                            keras.layers.Conv2D(64, kernel_size: 7, strides: 2, padding: "same", activation: "relu"),
                            keras.layers.BatchNormalization(),
                            keras.layers.LeakyReLU(),
                            keras.layers.MaxPooling2D(pool_size: 3, strides: 2, padding: "same"),
                        }, "BlocksLayer_1");
+            */
+            x = keras.layers.Conv2D(64, kernel_size: 7, strides: 2, padding: "same", activation: "relu").Apply(x);
+            x = keras.layers.BatchNormalization().Apply(x);
+            x = keras.layers.LeakyReLU().Apply(x);
+            x = keras.layers.MaxPooling2D(pool_size: 3, strides: 2, padding: "same").Apply(x);
 
-            var b2 = new ResnetBlock(64, 2, true);
-            var b3 = new ResnetBlock(128, 2);
-            var b4 = new ResnetBlock(256, 2);
-            var b5 = new ResnetBlock(512, 2);
+            /*
+                        var b2 = new ResnetBlock(64, 2, true);
+                        var b3 = new ResnetBlock(128, 2);
+                        var b4 = new ResnetBlock(256, 2);
+                        var b5 = new ResnetBlock(512, 2);
+            */
+            x = ResnetBlock(64, 2, x, true);
+            x = ResnetBlock(128, 2, x);
+            x = ResnetBlock(256, 2, x);
+            x = ResnetBlock(512, 2, x);
+
+            x = keras.layers.GlobalAveragePooling2D().Apply(x);
+            Tensors outputs = keras.layers.Dense(config.NumberOfClass).Apply(x);
+
+
+            /*
+
+                                    var model = keras.Sequential(new[] {
+                                        b1, b2, b3, b4,b5,
+                                        keras.layers.GlobalAveragePooling2D(),
+                                        keras.layers.Dense(config.NumberOfClass),
+                                    });
+                         */
+
+
+            // Create the model
+            var model = keras.Model(inputs, outputs, "ResNet");
+            model.build(input_shape: input_shape);
 
             
-            var model = keras.Sequential(new[] {
-                b1, b2, b3, b4,b5,
-                keras.layers.GlobalAveragePooling2D(),
-                keras.layers.Dense(config.NumberOfClass),
-            });
 
             model.summary();
 
@@ -136,6 +244,9 @@ namespace SciSharp.Models.ImageClassification.Zoo
             model.compile(optimizer, loss, new[] { "accuracy" });
 
             return model;
+
         }
+
     }
+
 }
